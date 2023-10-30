54a6a753b2488a95359c960da3f0f15187eab016
Batchuluun0605 <23LP2014@nest.edu.mn>
2023-10-30 18:11:51 +0800

    add js

--- todo/index.js
+++ todo/index.js
@@ -1,17 +1,9 @@
-const body = document.querySelector('body');
-const container = document.createElement('div');
-const boards = document.createElement('div');
-const text = document.createElement('text')
-const buttons = document.createElement('button')
-
-container.classList.add('container')
-boards.classList.add('board')
-text.classList.add('text')
-
-body.appendChild(container)
-container.appendChild(boards)
-boards.appendChild(text)
-boards.appendChild(buttons)
-
-text.innerHTML = "<h2>To do 0</h2>"
-buttons.innerText = " +  Add card"
+let body = document.querySelector('body')
+let taskcontainer = document.querySelector('.taskcontainer')
+let container = document.querySelector('.container')
+let boards = document.querySelector('.boards')
+let buttons = document.querySelector('button')
+buttons.onclick = tab
+function tab() {
+    taskcontainer.style.display = "flex"
+}
