c69b8ed841d0d17cee0e3a6d3fb808f8705dbb24
Batchuluun0605 <23LP2014@nest.edu.mn>
2023-10-31 22:50:08 +0800

    add drag and drop

--- todo/index.js
+++ todo/index.js
@@ -1,11 +1,12 @@
 let body = document.querySelector('body');
 let taskcontainer = document.querySelector('.taskcontainer');
 let container = document.querySelector('.container');
-let boards = document.querySelector('.boards');
+let box = document.querySelector('.box');
 let buttons = document.querySelectorAll('.add-card');
-let away = document.querySelector(".away");
+let dark = document.querySelector(".dark");
 let addtask = document.querySelector('.addtask');
-let create = document.querySelector(".create")
+let create = document.querySelector(".create");
+let empty = document.querySelector(".empty")
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].onclick = tab
@@ -19,6 +20,16 @@ function closetask() {
 function add() {
     create.style.display = "flex"
 }
-away.onclick = closetask;
+
+dark.onclick = closetask;
 addtask.onclick = add;
 
+create.addEventListener("dragstart", function(event){
+    console.log(event);
+})
+empty.addEventListener("dragover", function(event){
+    event.preventDefault()
+})
+empty.addEventListener("drop", function(event){
+    empty.prepend(create)
+})
