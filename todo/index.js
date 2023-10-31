b0fd2dd5e9fcbb87336f42d647d49ecf52a918f2
Batchuluun0605 <23LP2014@nest.edu.mn>
2023-10-31 12:33:26 +0800

    js, css

--- todo/index.js
+++ todo/index.js
@@ -1,9 +1,24 @@
-let body = document.querySelector('body')
-let taskcontainer = document.querySelector('.taskcontainer')
-let container = document.querySelector('.container')
-let boards = document.querySelector('.boards')
-let buttons = document.querySelector('button')
-buttons.onclick = tab
-function tab() {
-    taskcontainer.style.display = "flex"
+let body = document.querySelector('body');
+let taskcontainer = document.querySelector('.taskcontainer');
+let container = document.querySelector('.container');
+let boards = document.querySelector('.boards');
+let buttons = document.querySelectorAll('.add-card');
+let away = document.querySelector(".away");
+let addtask = document.querySelector('.addtask');
+let create = document.querySelector(".create")
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = tab
 }
+function tab() {
+   taskcontainer.style.display = "block"; 
+}
+function closetask() {
+    taskcontainer.style.display = "none"
+}
+function add() {
+    create.style.display = "flex"
+}
+away.onclick = closetask;
+addtask.onclick = add;
+
