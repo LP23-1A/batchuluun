d3fee5ac8e7a49d4b7560267667ae155d6435aba
Batchuluun0605 <23LP2014@nest.edu.mn>
2023-11-01 21:32:22 +0800

    add function

--- todo/index.js
+++ todo/index.js
@@ -1,35 +1,67 @@
-let body = document.querySelector('body');
-let taskcontainer = document.querySelector('.taskcontainer');
-let container = document.querySelector('.container');
-let box = document.querySelector('.box');
-let buttons = document.querySelectorAll('.add-card');
-let dark = document.querySelector(".dark");
-let addtask = document.querySelector('.addtask');
-let create = document.querySelector(".create");
-let empty = document.querySelector(".empty")
+const taskcontainer = document.querySelector(".taskcontainer");
+const dark = document.querySelector(".dark");
+let buttons = document.querySelectorAll(".addbtn")
 
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].onclick = tab
+function add() {
+    taskcontainer.style.display = "block"
 }
-function tab() {
-   taskcontainer.style.display = "block"; 
-}
-function closetask() {
+function closedark() {
     taskcontainer.style.display = "none"
 }
-function add() {
-    create.style.display = "flex"
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = add
+    
 }
+const data = [];
 
-dark.onclick = closetask;
-addtask.onclick = add;
+function render(data) {
+    const cards = document.getElementsByClassName("cards")[0];
+    const done = []
+    cards.innerHTML = ""
+    for (let i = 0; i < data.length; i++) {
+    cards.innerHTML += createCard(data[i]);
+    }
+}
+function addCard() {
+    const mockData = {
+      title: "",
+      desc: "",
+      status: "",
+      priority: "",
+    };
+    let input = document.querySelector("input")
+    let textarea = document.querySelector("textarea")
+    let select = document.querySelector("select")
+    mockData.title =input.value;
+    mockData.desc = textarea.value;
+    mockData.status = "done";
+    mockData.priority = select.value;
+    data.push(mockData);
+    render(data);
+    console.log(data);
 
-create.addEventListener("dragstart", function(event){
+}
+function createCard(card) {
+    const { title, desc, priority } = card;
+    return ` <div class="card">
+<button class="done">v</button>
+<div class="info">
+<p>${title}</p>
+<span>${desc}</span>
+<div class="priority">${priority}</div>
+</div>
+<button class="close">x</button>
+</div>`;
+}
+render(data);
+let cards = document.querySelector(".cards")
+let empty = document.querySelector(".empty")
+cards.addEventListener("dragstart", function(event){
     console.log(event);
 })
 empty.addEventListener("dragover", function(event){
     event.preventDefault()
 })
 empty.addEventListener("drop", function(event){
-    empty.prepend(create)
+    empty.prepend(cards)
 })
