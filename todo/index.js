4bf170a26b7ccba4c411ed307646b6c8ff021f09
Batchuluun0605 <23LP2014@nest.edu.mn>
2023-11-03 12:00:36 +0800

    add js

--- todo/index.js
+++ todo/index.js
@@ -1,73 +1,77 @@
 const taskcontainer = document.querySelector(".taskcontainer");
 const dark = document.querySelector(".dark");
-let buttons = document.querySelectorAll(".addbtn")
+let buttons = document.querySelectorAll(".addbtn");
 
 function add() {
-    taskcontainer.style.display = "block"
+  taskcontainer.style.display = "block";
 }
 function closedark() {
-    taskcontainer.style.display = "none"
+  taskcontainer.style.display = "none";
 }
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].onclick = add
-    
+  buttons[i].onclick = add;
 }
-const data = [];
+let data = [];
 
 function render(data) {
-    const cards = document.getElementsByClassName("cards")[0];
-    cards.innerHTML = ""
-    for (let i = 0; i < data.length; i++) {
-        cards.innerHTML += createCard(data[i]);
-    }
+  const cards = document.getElementsByClassName("cards")[0];
+  cards.innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    cards.innerHTML += createCard(data[i]);
+  }
 }
 function addCard() {
-    const mockData = {
-      title: "",
-      desc: "",
-      status: "",
-      priority: "",
-    };
-    let input = document.querySelector("input")
-    let textarea = document.querySelector("textarea")
-    let select = document.querySelector("select")
-    mockData.title =input.value;
-    mockData.desc = textarea.value;
-    mockData.status = "done";
-    mockData.priority = select.value;
-    data.push(mockData);
-    render(data);
-    taskcontainer.style.display = "none"
+  const uid = Math.random();
+  console.log(uid);
+  const mockData = {
+    id: uid,
+    title: "",
+    desc: "",
+    status: "",
+    priority: "",
+  };
+
+  let input = document.querySelector("input");
+  let textarea = document.querySelector("textarea");
+  let select = document.querySelector("select");
+  mockData.title = input.value;
+  mockData.desc = textarea.value;
+  mockData.status = "done";
+  mockData.priority = select.value;
+  data.push(mockData);
+  render(data);
+  taskcontainer.style.display = "none";
 }
 function createCard(card) {
-    cards.innerHTML = ""
-    const { title, desc, priority } = card;
-    return ` <div class="card" draggable="true">
-<button class="done"><img src="https://icons8.com/icon/OyGfrOzh4XAT/select-checkmark-symbol-to-choose-true-answer" width="24px" height="4px" alt=""></button>
+  cards.innerHTML = "";
+  const { title, desc, priority, id } = card;
+  return ` <div id="${id}" class="card" draggable="true">
+<button class="done">v</button>
 <div class="info">
 <p>${title}</p>
 <span>${desc}</span>
 <div class="priority">${priority}</div>
 </div>
-<button class="close" onclick="closeBtn()">x</button>
+<button class="close" onclick= "closeBtn(${id})">x</button>
 </div>`;
-
 }
 render(data);
-let cards = document.querySelector(".cards")
-let empty = document.querySelector(".empty")
-card.addEventListener("dragstart", function(event){
-    console.log(event);
-})
-empty.addEventListener("dragover", function(event){
-    event.preventDefault()
-})
-empty.addEventListener("drop", function(event){
-    empty.prepend(cards)
-})
-let card = document.querySelector(".card")
-let close = document.querySelectorAll(".close")
+let cards = document.querySelector(".cards");
+let empty = document.querySelector(".empty");
+cards.addEventListener("dragstart", function (event) {
+  console.log(event);
+});
+empty.addEventListener("dragover", function (event) {
+  event.preventDefault();
+});
+empty.addEventListener("drop", function (event) {
+  empty.prepend(cards);
+});
 
-function closeBtn() {
-    cards.style.display = "none"
+let card = document.querySelector(".card");
+let close = document.querySelector(".close");
+function closeBtn(id) {
+  const a = data.filter((e) => e.id !== id);
+  data = a;
+  render(data);
 }
