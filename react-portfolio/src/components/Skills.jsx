150cdb2fe463456fa753abb6830f8309f9533839
Batchuluun0605 <23LP2014@nest.edu.mn>
2023-11-20 22:10:38 +0800

    add

---
+++ react-portfolio/src/components/Skills.jsx
@@ -0,0 +1,13 @@
+import Technology from "./Technology";
+
+export default function Skill() {
+  <div>
+    <div>
+      <button>Skills</button>
+      <p>The skills, tools and technologies I am really good at:</p>
+    </div>
+    <div>
+      <Technology />
+    </div>
+  </div>;
+}
--- react-portfolio/src/pages/index.js
+++ react-portfolio/src/pages/index.js
@@ -1,11 +1,15 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Skill from "@/components/Skills";
+import Introduction from "@/components/introduction";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Introduction />
+      <Skill />
     </div>
   );
 }
