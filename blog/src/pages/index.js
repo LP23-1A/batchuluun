cb523416dfaf6536be8766840d0dec9b67aee09a
Batchuluun0605 <23LP2014@nest.edu.mn>
2023-12-05 12:42:32 +0800

    a

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -8,10 +8,30 @@ export default function Home() {
         <Navbar />
       </section>
       <section>
-        <Trend />
+        <div className="flex flex-col gap-7">
+          <div>
+            <h2 className="text-2xl">Trending</h2>
+          </div>
+          <Trend />
+        </div>
       </section>
       <section>
-        <Blog />
+        <div className="flex flex-col gap-8">
+          <h2 className="text-2xl">All Blog Post</h2>
+          <div className="flex justify-between">
+            <ul className="flex gap-5">
+              <li className=" text-yellow-400">All</li>
+              <li>Design</li>
+              <li>Travel</li>
+              <li>Fashion</li>
+              <li>Technology</li>
+              <li>Branding</li>
+            </ul>
+            <p>view All</p>
+          </div>
+
+          <Blog />
+        </div>
       </section>
     </div>
   );
