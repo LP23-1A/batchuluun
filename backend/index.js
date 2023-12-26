096f9cf180a7245f3ccfaaea3f6d48e4866a0b29
Batchuluun0605 <23LP2014@nest.edu.mn>
2023-12-26 17:47:51 +0800

    a

--- backend/index.js
+++ backend/index.js
@@ -1,54 +1,69 @@
-// import { Pool } from "pg";
-
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-
-//   ssl: true,
-// });
-
-// async function getData() {
-//   const client = await pool.connect();
-
-//   try {
-//     const response = await client.query("SELECT version()");
-
-//     console.log(response.rows[0]);
-
-//     return response.rows[0];
-//   } finally {
-//     client.release();
-//   }
-// }
-
-// export default async function Page() {
-//   const data = await getData();
-// }
-import Express from "express";
+import express from "express";
 import dotenv from "dotenv";
 import bp from "body-parser";
-// import  query from "/db";
 import { pool } from "./db.js";
-const app = Express();
+
 dotenv.config();
-app.use(bp.json);
-const PORT = 8000;
-// app.use("/", (req, res) => {
-//   pool.query();
-// });
+const PORT = 5432;
+const app = express();
+
+app.use(bp.json());
+
 app.listen(PORT, () => {
-  console.log("localhost : ");
+  console.log(`App running on port ${PORT}.`);
 });
-app.get("/user", async (req, res) => {});
-app.post("/user", async (req, res) => {
+
+app.get("/user", async (req, res) => {
   try {
-    const tableQueryText = `
-   CREATE TABLE IF NOT EXIST users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT FULL,
-    email VARCHAR(255) NOT FULL
-   )`;
+    const queryText = `SELECT * FROM users`;
+    const response = await pool.query(queryText);
+    res.send(response.rows);
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 });
-app.post;
+
+app.get("/users", async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const queryText = `SELECT * FROM users WHERE name='${name}' AND email='${email}'`;
+    const response = await pool.query(queryText);
+    res.send(response.rows);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.post("/createTable", async (_, res) => {
+  try {
+    const tableQueryText = `
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL,
+    )`;
+    await pool.query(tableQueryText);
+    res.send("ok");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.post("/user", async (req, response) => {
+  const { name, email } = req.body;
+  console.log(name, email, "req.body");
+  try {
+    const queryText =
+      "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *";
+    const res = await pool.query(queryText, [name, email]);
+    response.send(res.rows[0]);
+  } catch (error) {
+    console.error(error);
+    response.send("error query");
+  }
+});
+
+// `
+//     INSERT INTO users (name, email) VALUES ($1, $2)
+//     INSERT INTO tableName (fields) Values ()
+// `
